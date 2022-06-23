b80dd886ba922837b6db09ad7f7d77826b472366
Mazdak <nazemi.works@gmail.com>
2022-06-23 18:36:42 +0200

    more on

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,86 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
-import styles from '../styles/Home.module.css'
+// ---
+
+import styles from "../styles/Home.module.css";
+import { WrapperStyled } from "../styles/pages";
+
+// ---
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <meta name="text" content="nane" />
-        <meta name="description" content="description" />
-        <meta name="keywords" content="keywords" />
-        <title>Penbeh - Mazdak</title>
+        <title>Mazdak</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {/* <LayoutProvider>
-        <HomeProvider />
-      </LayoutProvider> */}
-      <div className={styles.main}>
-        <div>in persian we say : the potter drink from broken jar!</div>
-        <div>currently dont have time to make a beauty for myself</div>
-        <div>mail : nazemi.works@gmail.com</div>
-        <div>Best!</div>
+      <div className={styles.container}>
+        <WrapperStyled>
+          <div className="first">
+            <div className="img">
+              <Image
+                style={{
+                  borderRadius: "1rem",
+                  marginLeft: "1rem",
+                  border: "2px solid #15202B",
+                }}
+                layout="responsive"
+                src="/MK.jpeg"
+                alt="Picture of the author"
+                width={"380rem"}
+                height={"380rem"}
+              />
+            </div>
+            <div className="texi">
+              <h1>Mazdak Nazemi </h1>
+              <h2>Full Stack Developer</h2>
+              <h3>
+                Living In Germany[&nbsp;"baden württemberg"&nbsp;].Heidelberg
+              </h3>
+              <h5>Iranian from 1989</h5>
+            </div>
+          </div>
+          <div className="second">
+            <div className="desc">
+              <p>
+                this is Mazdak like Mazda (that car brand) with k at end easy to
+                pronounce , already delivered some nice products to people for
+                people my mindset is like Architecture , engineering over
+                scripting , no rush for dirty hands , more into to be a techi ,
+                I can code with Javascript , Typescrip (typo) , Go and also dear Rust
+                upcoming one , with thier related libraries , frameworks and
+                stuff to them , and ...
+              </p>
+            </div>
+            <div className="tags">
+              <a target="_blank" href="https://devref.ir">
+                Mein Boilers
+              </a>
+              <a target="_blank" href="https://github.com/penkong">
+                Mein Github
+              </a>
+              <a target="_blank" href="https://gitlab.com/mk-nazemi">
+                Mein Gitlab
+              </a>
+              <a
+                target="_blank"
+                href="https://stackoverflow.com/users/11584130/mazdak"
+              >
+                Mein Snackoverlow
+              </a>
+              <a href="MazdakNazemi_CV.pdf" title="Mazdak Nazemi CV" download>
+                Mein Resume
+              </a>
+              <p>Email : Nazemi.works@gmail.com</p>
+              <a href=""></a>
+            </div>
+          </div>
+        </WrapperStyled>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ styles/pages/Home.styled.ts
@@ -0,0 +1,161 @@
+import styled from "styled-components";
+
+export const WrapperStyled = styled.div`
+  width: 96%;
+  height: 90%;
+  border-radius: 1rem;
+  border: 2px solid #15202b;
+  box-shadow: 1px;
+  background-color: #f1f1f1;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .first {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: -6rem;
+    color: white;
+    background-color: #15202b;
+    border-radius: 0.5rem;
+    width: 90%;
+    height: 40%;
+    .img {
+      width: 25rem;
+    }
+    .texi {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      height: 8rem;
+      width: auto;
+      h1 {
+        color: #00f7ff;
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .second {
+    width: 90%;
+    margin-bottom: -7rem;
+    margin-top: 3rem;
+    height: 40%;
+    border-radius: 0.5rem;
+    background-color: #15202b;
+    color: white;
+    padding-left: 2rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    .desc {
+      height: 60%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      line-height: 2rem;
+      width: 55%;
+    }
+
+    .tags {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 60%;
+      width: 20%;
+      a {
+        &:hover {
+          color: #00f7ff;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 1189px) {
+    justify-content: space-evenly;
+    border: 0px ;
+    .first {
+      margin-top: 0rem;
+      flex-direction: column;
+      justify-content: space-around;
+      .img {
+        width: 15rem;
+        margin-top: -3rem;
+      }
+      .texi {
+        font-size: 0.8rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        max-height: 5rem;
+      }
+    }
+    .second {
+      height: 50%;
+      padding-left: 0;
+      justify-content: space-around;
+      align-items: center;
+      padding: 0 1rem;
+      flex-direction: column;
+      margin-bottom: 0;
+      margin-top: 0;
+      .desc {
+        line-height: 1.5rem;
+        width: 90%;
+      }
+      .tags {
+        width: 90%;
+      }
+    }
+  }
+
+  @media screen and (max-width: 708px) {
+    .first {
+      width: 96%;
+      .texi {
+        font-size: 0.6rem;
+        max-height: 7rem;
+        padding: 0 0.1rem;
+      }
+    }
+    .second {
+      width: 96%;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    .first {
+      width: 96%;
+      .img {
+        width: 12rem;
+      }
+      .texi {
+        font-size: 12px;
+        h3 {
+          font-size: 9px;
+        }
+        height: 7rem;
+        width: 90%;
+      }
+    }
+    .second {
+      justify-content: space-evenly;
+      .desc {
+        font-size: 15px;
+        line-height: 1.4rem;
+        width: 96%;
+      }
+      .tags {
+        font-size: 14px;
+        height: 9rem;
+        width: 90%;
+      }
+    }
+  }
+`;
